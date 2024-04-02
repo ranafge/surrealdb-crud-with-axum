4a502838e38b71f9c35e5fb9819b272e5478c0ff
ranafge <ranafge@gmail.com>
2024-04-03 00:11:36 +0600

    from submisstion

--- src/modules/form.rs
+++ src/modules/form.rs
@@ -18,14 +18,28 @@ pub async fn show_from() -> Html<&'static str> {
                 <title>SurrealDB</title>
             </head>
             <body>
-                <form method="post" action="/show_form">
-                    <label for="name">Enter your name</label>
-                    <input type="text" name="name" id="name">
-                    <label for="email">Enter your email</label>
-                    <input type="text" name="email" id="email">
-                    <input type="submit" value="Subscribe!">
-                </form>
-                <h2>Thanks for submission</h2>
+            <h2>District Form</h2>
+            <form action="/show_form" method="post">
+                <label for="name">Name:</label><br>
+                <input type="text" id="name" name="name"><br>
+                
+                <label for="population">Population:</label><br>
+                <input type="number" id="population" name="population"><br>
+                
+                <label for="climate">Climate:</label><br>
+                <input type="text" id="climate" name="climate"><br>
+                
+                <label for="international_border">International Border:</label><br>
+                <input type="checkbox" id="international_border" name="international_border"><br>
+                
+                <label for="country">Country:</label><br>
+                <input type="text" id="country" name="country"><br>
+                
+                <label for="immediate_upper_division">Immediate Upper Division:</label><br>
+                <input type="text" id="immediate_upper_division" name="immediate_upper_division"><br><br>
+                
+                <input type="submit" value="Submit">
+            </form>
             </body>
         </html>
         "#
